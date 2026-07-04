e6c46dcb209d37ae174ce5741a76842f708c02e1
应卓 <yingzhor@gmail.com>
2026-07-04 11:15:21 +0800

    feat(user): UserLoginEventListener 改为异步事件监听

--- projects-app/core/src/main/java/io/github/yingzhuo/claude/core/m/user/eventlistener/UserLoginEventListener.java
+++ projects-app/core/src/main/java/io/github/yingzhuo/claude/core/m/user/eventlistener/UserLoginEventListener.java
@@ -4,6 +4,7 @@ import io.github.yingzhuo.claude.core.m.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,7 @@ public class UserLoginEventListener {
 	 *
 	 * @param event 用户登录成功事件
 	 */
+	@Async
 	@EventListener
 	@Transactional
 	public void handleUserLoginSuccess(UserLoginSuccessEvent event) {
